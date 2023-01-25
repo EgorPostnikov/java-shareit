a800f776fc6a0549b08579a7651348d14dc13d14
Egor <e.onator@yandex.ru>
2023-01-25 21:56:41 +0300

    Feat: Add Logs

---
+++ src/main/java/ru/practicum/shareit/booking/model/BookedItem.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.booking.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "items", schema = "public")
+public class BookedItem {
+    @Id
+    private Long id;
+    private String name;
+    @Column(name = "owner_id")
+    private Long owner;
+}
