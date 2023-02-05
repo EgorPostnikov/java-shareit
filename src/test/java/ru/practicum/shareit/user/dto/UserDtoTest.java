56ade75d517d8e67506f12d5c3e69f983281cf50
Egor <e.onator@yandex.ru>
2023-02-06 01:38:59 +0300

    Feat: add test

--- src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
+++ src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.user.controller.dto;
+package ru.practicum.shareit.user.dto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
