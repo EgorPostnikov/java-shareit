b1f056799d55a3f739eabb6588efdc94a1a408a7
Egor <e.onator@yandex.ru>
2023-02-13 16:43:34 +0300

    Feat: add user on gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/gateway/user/UserDto.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.gateway.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.shareit.gateway.validation.Create;
+import ru.practicum.shareit.gateway.validation.Update;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String name;
+    @NotNull(message = "Email is required.", groups = {Create.class})
+    @Email(message = "Invalid email.", groups = {Create.class, Update.class})
+    //@UsedEmailValidation(groups = {Create.class})
+    private String email;
+}
